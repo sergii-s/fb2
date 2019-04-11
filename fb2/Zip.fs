10651e37ee5c9e5fad082b41f77fdddb4b6a45a1
sergii <sergii@antvoice.com>
2019-04-11 10:39:42 +0200

    unzip file by file to be able to replace existing files

--- fb2/Zip.fs
+++ fb2/Zip.fs
@@ -6,11 +6,20 @@ module Zip =
 
     let zip rootFolder targetFile files = 
         use zipFile = new FileStream(targetFile, FileMode.Create)
-        use archive = new ZipArchive(zipFile, ZipArchiveMode.Update)
+        use archive = new ZipArchive(zipFile, ZipArchiveMode.Create)
         for file in files do
             let relativeFilePath = file |> Pathes.toRelativePath rootFolder
             archive.CreateEntryFromFile(file, relativeFilePath) |> ignore
         targetFile
 
-    let unzip targetDirectory zipFile =
-        ZipFile.ExtractToDirectory(zipFile, targetDirectory)
+    let unzip targetDirectory zipFileName =
+        use zipFile = new FileStream(zipFileName, FileMode.Open)
+        use archive = new ZipArchive(zipFile, ZipArchiveMode.Read)
+        for file in archive.Entries do
+            let completeFileName = Path.Combine(targetDirectory, file.FullName)
+            let directory = Path.GetDirectoryName(completeFileName)
+            if Directory.Exists(directory) |> not then
+                Directory.CreateDirectory(directory) |> ignore
+            if file.Name <> "" then
+                file.ExtractToFile(completeFileName, true)
+        
