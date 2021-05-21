c13e83627a113d05f8570de051d49db27fccbf6b
bastien <bastien@antvoice.com>
2021-05-21 15:21:50 +0200

    Remove useless pipeline step

--- add-source.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dotnet nuget add source https://packages.antvoice.com/repository/nuget-hosted/ \
-	-n antvoice-nuget-hosted -u ${USERNAME} -p ${PASSWORD} \
-	--store-password-in-clear-text
